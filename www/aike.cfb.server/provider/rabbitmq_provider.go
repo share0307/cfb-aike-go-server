ec368f813ce212ea5024df11c3f3e033a4b20d40
Kwin <kwin@promote.cache-dns.local>
2021-04-21 12:53:15 +0800

    实现消费者

--- www/aike.cfb.server/provider/rabbitmq_provider.go
+++ www/aike.cfb.server/provider/rabbitmq_provider.go
@@ -20,7 +20,7 @@ func NewRabbitmqProvider(alias string)  *RabbitmqProvider {
 
 	// 返回mq实例
 	mqProvider := &RabbitmqProvider{
-		Config: mqConfig,
+		config	: mqConfig,
 	}
 
 	// 启动监听
@@ -47,7 +47,7 @@ func NewRabbitmqProvider(alias string)  *RabbitmqProvider {
  */
 type RabbitmqProvider struct {
 	// 保存使用的配置别名
-	Config config.RabbitmqConfig
+	config config.RabbitmqConfig
 	// 保存rabbitmq的链接
 	connect *amqp.Connection
 	// 保存rabbitmq的渠道
@@ -60,11 +60,11 @@ type RabbitmqProvider struct {
 func (r *RabbitmqProvider)Connect()  {
 	var err error
 	var amqpConfig = amqp.Config{
-		Heartbeat	:		time.Duration(r.Config.Heartbeat) * time.Second,
-		Vhost				: 		r.Config.Vhost,
+		Heartbeat	:		time.Duration(r.config.Heartbeat) * time.Second,
+		Vhost				: 		r.config.Vhost,
 	}
 
-	r.connect,err = amqp.DialConfig(r.Config.Dsn, amqpConfig)
+	r.connect,err = amqp.DialConfig(r.config.Dsn, amqpConfig)
 
 	if err != nil {
 		panic("链接mq失败，原因为：" + err.Error())
@@ -86,8 +86,8 @@ func (r *RabbitmqProvider)Connect()  {
 func (r *RabbitmqProvider)InitExchangeAndQueue() {
 	// 声明交换机
 	r.channel.ExchangeDeclare(
-		r.Config.Exchange,
-		r.Config.ExchangeType,
+		r.config.Exchange,
+		r.config.ExchangeType,
 		true,
 		false,
 		false,
@@ -97,7 +97,7 @@ func (r *RabbitmqProvider)InitExchangeAndQueue() {
 
 	// 声明队列
 	r.channel.QueueDeclare(
-		r.Config.Queue,
+		r.config.Queue,
 		true,
 		false,
 		false,
@@ -107,9 +107,9 @@ func (r *RabbitmqProvider)InitExchangeAndQueue() {
 
 	// 帮顶队列与交换机的关系
 	r.channel.QueueBind(
-		r.Config.Queue,
-		r.Config.Route,
-		r.Config.Exchange,
+		r.config.Queue,
+		r.config.Route,
+		r.config.Exchange,
 		true,
 		nil,
 		)
@@ -123,9 +123,30 @@ func (r *RabbitmqProvider)Publish(msg amqp.Publishing) error {
 	//                           当 mandatory 参数为 false 时，出现上述情形，则消息直接被丢弃
 	// immediate：当immediate参数设为true时，如果交换器在将消息路由到队列时发现队列上并不存在任何消费者，那么这条消息将不会存入队列中。当与路由键匹配的所有队列都没有消费者时，该消息会通过Basic.Return返回至生产者。
 
-	err := r.channel.Publish(r.Config.Exchange, r.Config.Route, false, false, msg)
+	err := r.channel.Publish(r.config.Exchange, r.config.Route, false, false, msg)
 
 	return err
+}
+
+/**
+	消费数据
+ */
+func (r *RabbitmqProvider)Consume(msg amqp.Publishing) (<- chan amqp.Delivery, error) {
+	deliveryChan,err := r.channel.Consume(
+		r.config.Queue,
+		"",
+		false,		// 是否自动应答
+		false,		// exclusive：是否排外的，有两个作用，一：当连接关闭时connection.close()该队列是否会自动删除；二：该队列是否是私有的private，如果不是排外的，可以使用两个消费者都访问同一个队列，没有任何问题，如果是排外的，会对当前队列加锁，其他通道channel是不能访问的，如果强制访问会报异常：com.rabbitmq.client.ShutdownSignalException: channel error; protocol method: #method<channel.close>(reply-code=405, reply-text=RESOURCE_LOCKED - cannot obtain
+		false,		// 设置为true，表示 不能将同一个Conenction中生产者发送的消息传递给这个Connection中 的消费者
+		false,		// nowait：是否等待服务器返回
+		nil,
+		)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return deliveryChan, err
 }
 
 /**
